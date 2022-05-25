644b39425f2d69eb0a51df8e289495dfc536b740
Arun Vijayarengan <arunprabu.v@hotmail.com>
2022-05-25 21:50:48 +0530

    updated

--- src/app/about/about.component.spec.ts
+++ src/app/about/about.component.spec.ts
@@ -1,16 +1,17 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, tick } from '@angular/core/testing';
 
 import { AboutComponent } from './about.component';
 
+// if you have nested describe it will violate Single Responsibility Principle of Unit Testing
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AboutComponent ]
+      declarations: [AboutComponent]
     })
-    .compileComponents();
+      .compileComponents();
   });
 
   beforeEach(() => {
@@ -30,5 +31,33 @@ describe('AboutComponent', () => {
   it('has About Us in h2', () => {
     const template = fixture.nativeElement as HTMLElement;
     expect(template.querySelector('h2')?.textContent).toContain('About Us');
+  });
+
+  // Approach #1
+  it('has London as city 1', fakeAsync(() => {
+    component.ngOnInit();
+    tick(3001);
+    expect(component.city).toEqual('London');
+  }));
+
+  // Approach #1
+  it(`has London as City 2 `, ((done) => {
+    console.log('### start test');
+    fixture.detectChanges();
+    // call a method which has async code
+    component.ngOnInit();
+    setTimeout(() => {
+      console.log('isStable', fixture.isStable());
+      console.log('### end test');
+      expect(component.city).toEqual('London');
+      done();
+    }, 3001)
+  }));
+
+  // test whether the element has bg color yellow
+  it('should have yellow bg in <h2>', () => {
+    const h2: HTMLElement = fixture.nativeElement.querySelector('h2');
+    const bgColor = h2.style.backgroundColor;
+    expect(bgColor).toBe('yellow');
   });
 });
--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -4,20 +4,30 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-about',
   template: `
     <div>
-      <h2>{{pageName}}</h2>
+      <h2 style="background-color: yellow;">{{pageName}}</h2>
     </div>
   `,
   styles: [
+    `.heading2{
+      background-color: 'yellow';
+    }
+    `
   ]
 })
 export class AboutComponent implements OnInit {
 
   pageName = '';
+
+  city = '';
   
   constructor() { }
 
   ngOnInit(): void {
     this.pageName = 'About Us';
+    setTimeout(()=>{
+      this.city = 'London';
+    }, 3000);
+    
   }
 
 }
